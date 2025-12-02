42f324528007e557dcc3c85edaf59433b0c8345b
Ilia Ilmer <contact@iliailmer.com>
2025-12-01 21:57:16 -0500

    part 2

--- day_1.py
+++ day_1.py
@@ -30,13 +30,20 @@ def part_2(puzzle: str) -> int:
     dial = 50
     for line in puzzle.split("\n"):
         direction, num = line[0], int(line[1:])
+        quot = num // 100
+        rem = num % 100
         if direction == "L":
-            dial -= num
+            pw += quot
+            if dial > 0 and dial - rem < 0:
+                pw += 1
+            dial = dial - num
         if direction == "R":
-            dial += num
-        if dial < 0 or dial > 100:
-            pw += 1
-        dial = dial % 100
+            if dial < 100 and dial + num > 100:
+                pw += quot
+            if dial < 100 and dial + rem > 100:
+                pw += 1
+            dial = dial + num
+        dial %= 100
         if dial == 0:
             pw += 1
     return pw
@@ -45,4 +52,6 @@ def part_2(puzzle: str) -> int:
 if __name__ == "__main__":
     with open("inputs/day_1/puzzle_1.txt", encoding="utf8") as f:
         puzzle = f.read().strip()
+    # puzzle = """R150\nL50"""
     print(part_1(puzzle))
+    print(part_2(puzzle))
